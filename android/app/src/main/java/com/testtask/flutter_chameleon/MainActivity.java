dab673f5a996404fadd1d8ca8b62631150cf97da
morbillius <morbillius@gmail.com>
2019-12-18 21:00:37 +0200

    simple final

---
+++ android/app/src/main/java/com/testtask/flutter_chameleon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.testtask.flutter_chameleon;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chameleon',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Chameleon Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color _color = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _color,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: new Stack(
+        children: <Widget>[
+          Center(
+            child: Text(
+              'Hey there',
+            ),
+          ),
+          Container(
+              child: new GestureDetector(
+                onTap: () => _toggleColor(),
+              ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _toggleColor() {
+    Random random = new Random();
+    setState(() =>
+        _color = Color.fromARGB(255, random.nextInt(255), random.nextInt(255), random.nextInt(255))
+    );
+  }
+}
